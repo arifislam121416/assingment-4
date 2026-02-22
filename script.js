a034a1579198488f670c6200da0b1c24477cecb7
arifislam121416 <ardairyhelthcare@gmail.com>
2026-02-22 17:42:38 +0600

    all interview reject button some work

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let interview = document.getElementById("interview-count");
 let reject = document.getElementById("reject-count");
 
 const allCards = document.getElementById("all-card");
+const mainContainer =document.getElementById("containet");
 
 function calculateCount (){
     total.innerText = allCards.children.length ;
@@ -20,13 +21,21 @@ const allFilter = document.getElementById("all-btn");
 const interviewFilter = document.getElementById("interview-btn");
 const rejectFilter = document.getElementById("reject-btn");
 
-function styleList(id){
+function toggleStyle(id){
+ allFilter.classList.add('bg-black-300','text-white')
+ interviewFilter.classList.add('bg-black-300','text-white')
+ rejectFilter.classList.add('bg-black-300','text-white')
 
+ allFilter.classList.remove('bg-black','text-white')
+ interviewFilter.classList.remove('bg-black','text-white')
+ rejectFilter.classList.remove('bg-black','text-white')
+
+
+
+ const selected = document.getElementById(id)
+currentStatus = id
+
+ selected.classList.remove('bg-gray-300', 'text-black')
+    selected.classList.add('bg-black', 'text-white')
 }
-
-
-
-// const interviewList = document.getElementById("interview-btn");
-// const rejectList = document.getElementById("reject-btn");
-// const allList = document.getElementById("all-btn");
 
